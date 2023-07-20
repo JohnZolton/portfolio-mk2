5a00e144d9add70c1429bdf6179eaffc9b8d9c23
John <jgz6@uakron.edu>
2023-07-19 20:39:14 -0400

    email clipboard

--- src/pages/components/hero.tsx
+++ src/pages/components/hero.tsx
@@ -1,8 +1,30 @@
+import React from "react";
+import { MdEmail } from "react-icons/md";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 export const HeroSection = () => {
+  const copyToClipboard = async () => {
+    await navigator.clipboard.writeText('jgz6@uakron.edu');
+    toast("Email copied to clipboard!");
+  };
+
   return (
-    <div className="w-full max-w-lg  p-10">
+    <div className="w-full max-w-lg p-10">
       <div className="pb-3 text-5xl font-semibold">John Zolton</div>
       <div className="text-2xl">Software Engineer</div>
+      <button 
+        onClick={copyToClipboard} 
+        className="mt-3 flex items-center hover:text-white"
+      >
+        <MdEmail className="mr-1" size="1.5em"/>
+      </button>
+      <ToastContainer 
+      hideProgressBar={true}
+      theme="dark"
+      position="top-right" 
+      autoClose={1000}
+       />
     </div>
   );
 };
