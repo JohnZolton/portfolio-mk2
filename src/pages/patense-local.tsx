eaef1bb29c7c335a0644cdd2ba994bde026072d3
John <jgz6@uakron.edu>
2024-10-22 20:21:49 -0400

    project description updates

--- src/pages/patense-local.tsx
+++ src/pages/patense-local.tsx
@@ -15,7 +15,7 @@ function Patense() {
         <h1 className="mb-8 text-center text-6xl font-bold">Patense.local</h1>
         <div className="mb-4 flex items-center justify-center">
           <video width="100%" height="auto" controls muted>
-            <source src={"/lifterdemo.mp4"} type="video/mp4" />
+            <source src={"/extract.mp4"} type="video/mp4" />
             Your browser does not support the video tag.
           </video>
         </div>
@@ -39,19 +39,88 @@ function Patense() {
             100% private, local document analysis with LLMs.
           </p>
         </section>
-
-        <section className="mb-8">
-          <h2 className="mb-4 text-2xl font-semibold">Heading</h2>
-          <p className="text-lg leading-relaxed">todo</p>
-        </section>
-
         <section className="mb-8">
           <h2 className="mb-4 text-2xl font-semibold">Features</h2>
-          <ul className="list-disc space-y-2 pl-5">todo</ul>
+          <ul className="list-disc space-y-2 pl-5">
+            <li>Inventive Feature Extraction</li>
+            <li>Deep Reference Search</li>
+          </ul>
         </section>
-        <section className="mb-8">
-          <h2 className="mb-4 text-2xl font-semibold">Heading 2</h2>
-          <p className="text-lg leading-relaxed">todo</p>
+
+        <section className="mb-8 flex flex-col gap-y-4">
+          <h2 className="text-2xl font-semibold">AI Deep Search</h2>
+          <p className="text-lg leading-relaxed">
+            A lot of what patent attorney&apos;s do is search documents for
+            various features. The problem is that takes time and people get
+            tired/distracted. And an LLM can read WAY faster (hundreds/thousands
+            of words per second).
+          </p>
+          <p className="text-lg leading-relaxed">
+            So why not have the LLM search the documents? It&apos;s the same
+            idea as in{" "}
+            <Link className="underline" href={"patense"}>
+              Patense.ai
+            </Link>
+            . Enter a feature, break all the references into chunks (to increase
+            the accuracy of the LLM response) and ask the LLM if the chunk
+            discloses or suggests the feature (the standard for getting a
+            patent).
+          </p>
+          <p className="text-lg leading-relaxed">
+            Using this technique with a 3090, I was able to extract relevant
+            paragraphs from ~60 pages of patents in 40 seconds.
+          </p>
+        </section>
+        <p className="mb-8 text-lg leading-relaxed">
+          This &apos;reads&apos; all the documents and identified any sections
+          that may be relevant to your possible inventive feature. Since LLMs
+          hallucinate, I also included the source text as well for easy
+          verification.
+        </p>
+        <section className="mb-8 flex flex-col gap-y-4">
+          <h2 className="text-2xl font-semibold">
+            Inventive Feature Extraction
+          </h2>
+          <p className="text-lg leading-relaxed">
+            This uses a similar technique but searching your own Specification.
+          </p>
+          <p className="text-lg leading-relaxed">
+            This extracts every possible feature so you can get an overview of
+            potential strategies and easily search the references (your own
+            disclosure defines the possible features you can use to distinguish
+            your invention from the prior art).
+          </p>
+        </section>
+        <section className="mb-8 flex flex-col gap-y-4">
+          <h2 className="text-2xl font-semibold">The Tech</h2>
+          <p className="text-lg leading-relaxed">
+            Originally this used a naive queue, then I learned about vLLM and
+            continuous batching. Continuous batching is-hang on. In a graphics
+            card, there&apos;s space for the model and space for the prompt. If
+            there&apos;s space left over you can stuff in more prompts. When
+            those prompts are finished, space opens up. Continuous batching is
+            the process of automatically refilling the graphics card as prompts
+            finish and free memory. This led to massive (8x) speed gains.
+          </p>
+          <p className="text-lg leading-relaxed">
+            The original project,{" "}
+            <Link href={"patense"} className="underline">
+              Patense.ai
+            </Link>
+            , used batched concurrent api calls to chatGPT. I chose not to use
+            local because (1) the target user likely doesn&apos;t have the
+            compute or the knowhow to run it locally and (2) parallel API calls
+            were way faster.
+          </p>
+          <p className="text-lg leading-relaxed">
+            But with continuous batching, local LLMs have a ~0.5-1.0 second
+            response time (at least with an 8b model). Making it much faster
+            than openAI (at my current price tier).
+          </p>
+          <p className="text-lg leading-relaxed">
+            I think the entire process of obtaining a patent can be automated.
+            I&apos;m building out tools for the various macro actions needed.
+          </p>
         </section>
       </div>
     </ProjectPage>
--- src/pages/snorkle.tsx
+++ src/pages/snorkle.tsx
@@ -14,10 +14,13 @@ function Patense() {
       <div className="container mx-auto max-w-3xl p-2">
         <h1 className="mb-8 text-center text-6xl font-bold">Snorkle</h1>
         <div className="mb-4 flex items-center justify-center">
-          <video width="100%" height="auto" controls muted>
-            <source src={"/lifterdemo.mp4"} type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
+          <div className="mb-4 flex items-center justify-center">
+            <img
+              src="/snorkle.gif"
+              alt="Snorkling GIF"
+              className="h-auto w-full"
+            />
+          </div>
         </div>
         <div className="mx-auto my-2 flex max-w-xs flex-row items-center justify-center  px-10">
           <Link
@@ -33,25 +36,80 @@ function Patense() {
           Tech Stack:{" "}
           <StackDisplay stack="vLLM, Next.js, TypeScript, React, tRPC, Prisma" />
         </p>
-        <section className="mb-8">
-          <h2 className="my-4 text-2xl font-semibold">Snorkle</h2>
+        <section className="mb-8 ">
+          <div className="flex flex-row gap-x-2">
+            <h2 className="my-4 text-2xl font-semibold">Snorkle</h2>
+            <div className="transtion flex flex-row items-center duration-300 ">
+              <div className="flex flex-row items-start justify-start">
+                <div className="px-1 font-semibold">{24}</div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="lucide lucide-star"
+                >
+                  <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" />
+                </svg>
+                <div className="flex flex-row items-center transition duration-300 ">
+                  <div className="px-1 font-semibold">{4}</div>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="lucide lucide-git-fork"
+                  >
+                    <circle cx="12" cy="18" r="3" />
+                    <circle cx="6" cy="6" r="3" />
+                    <circle cx="18" cy="6" r="3" />
+                    <path d="M18 9v1a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2V9" />
+                    <path d="M12 12v3" />
+                  </svg>
+                </div>
+              </div>
+            </div>
+          </div>
           <p className="text-lg leading-relaxed">
             100% private, local document analysis with LLMs.
           </p>
         </section>
-
-        <section className="mb-8">
-          <h2 className="mb-4 text-2xl font-semibold">Heading</h2>
-          <p className="text-lg leading-relaxed">todo</p>
-        </section>
-
         <section className="mb-8">
           <h2 className="mb-4 text-2xl font-semibold">Features</h2>
-          <ul className="list-disc space-y-2 pl-5">todo</ul>
+          <ul className="list-disc space-y-2 pl-5">
+            <li>Deep Document Search</li>
+          </ul>
+        </section>
+
+        <section className="mb-8">
+          <p className="text-lg leading-relaxed">
+            This is a fork of{" "}
+            <Link className="underline" href={"patense-local"}>
+              Patense.local
+            </Link>{" "}
+            with a modified system prompt for generic document searches.
+            Sometimes ctl+F doesn&apos;t cut it and you need a broader or more
+            robust way to find relevant text.
+          </p>
         </section>
         <section className="mb-8">
-          <h2 className="mb-4 text-2xl font-semibold">Heading 2</h2>
-          <p className="text-lg leading-relaxed">todo</p>
+          <h2 className="mb-4 text-2xl font-semibold">Tech</h2>
+          <p className="text-lg leading-relaxed">
+            The core tech and features are the same. Break reference documents
+            into chunks and ask an LLM if the content is relevant to a given
+            query. Return a brief quote and the full text for verification. Uses
+            Next.js on the frontend with a vLLM server.
+          </p>
         </section>
       </div>
     </ProjectPage>
